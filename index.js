bc11a2f937e4cad17b7795220c136e4f7920c6ad
Thomas 'Panda' Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2017-10-21 14:19:31 +0100

    what type is the stream?

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.get("/ncco", function(req, res, next) {
 app.ws('/connect', function(ws, req) {
   ws.on('message', function(msg) {
     console.log(msg);
+    console.log(typeof msg);
     if (msg.type === 'utf8') {
       console.log(msg.utf8Data);
     } else if(msg.type === 'binary') {
