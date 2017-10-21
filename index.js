71ab63c6095d0c212cc58bd20611cfda7cf2d3e8
Thomas 'Panda' Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2017-10-21 13:02:37 +0100

    add echo bot

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+
+app.get("/ncco", function(req, res, next) {
+  res.sendFile("/ncco.json")
+});
+
+app.ws('/connect', function(ws, req) {
+  ws.on('message', function(msg) {
+    if (msg.type === 'utf8') {
+      console.log(message.utf8Data);
+    } else if(message.type === 'binary') {
+      console.log("Binary message recieved");
+      connection.sendBytes(message.binaryData);
+    }
+  });
+});
+
+app.listen(443);
