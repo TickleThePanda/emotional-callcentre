88893cba9bc01ce586962d3135407000771cb0e5
Thomas 'Panda' Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2017-10-22 02:45:09 +0100

    use real values for context

--- request-constants.js
+++ request-constants.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 module.exports.RIFF = fs.readFileSync(__dirname + "/riff.wav");
 module.exports.HEADER_SEPARATOR = "\r\n";
-module.exports.SETUP_REQUEST = '{"context":{"system":{"version":"2.0.12341"},"os":{"platform":"N/A","name":"N/A","version":"N/A"},"device":{"manufacturer":"N/A","model":"N/A","version":"N/A"}}}';
+module.exports.SETUP_REQUEST = '{"context":{"system":{"version":"2.0.12341"},"os":{"platform":"Linux","name":"Debian","version":"2.14324324"},"device":{"manufacturer":"Contoso","model":"Fabrikan","version":"7.341"}}}';
 module.exports.AUDIO_TYPE = "audio/x-wav";
 module.exports.JSON_TYPE = "application/json; charset=utf-8";
